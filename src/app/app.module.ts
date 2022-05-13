e7c79b0577888e065417008c34b9abd455004b92
Leandro Cavalcanti <leandrocavalcanti499@gmail.com>
2022-05-13 13:24:11 -0300

    Integrando com API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
 import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
